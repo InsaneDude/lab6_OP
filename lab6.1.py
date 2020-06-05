4505e5f7bb138bee9f5ce4efb49d47ab6d4d2828
insane_dude <insane.dude.btw@gmail.com>
2020-06-06 00:48:29 +0300

    sin and cos from var is working

--- lab6.1.py
+++ lab6.1.py
@@ -33,7 +33,7 @@ class CreatingVariable(MethodClass):
         used_variable = sym.Symbol(input("Введіть змінну, яку хочете використовувати : "))
         input_expr = input("Введіть вираз із вашою змінною : ")
         if str(used_variable) not in str(input_expr):
-            print("Змінна введена невірно.")
+            print("Змінна або вираз введені невірно.")
             quit()
         return input_expr
 
@@ -53,15 +53,33 @@ class CreatingVariable(MethodClass):
 class SinFromVariable(CreatingVariable):
     def sin_var(self):
         sin_from_variable = sym.sin(self.used_var())
-        print("sin від вашою змінної :", sin_from_variable)
+        print("sin від заданого виразу :", sin_from_variable)
         return sin_from_variable
 
 
 class DiffSinFromVariable(SinFromVariable):
     def sin_diff_var(self):
         sin_differed_var = sym.diff(self.sin_var())
-        print("Похідна від sin :", sin_differed_var)
+        print("Похідна від заданого виразу :", sin_differed_var)
 
 
-k = DiffSinFromVariable()
-k.sin_diff_var()
+class CosFromVariable(CreatingVariable):
+    def cos_var(self):
+        cos_from_variable = sym.cos(self.used_var())
+        print("cos від заданого виразу :", cos_from_variable)
+        return cos_from_variable
+
+
+class DiffCosFromVariable(CosFromVariable):
+    def cos_diff_var(self):
+        cos_differed_var = sym.diff(self.cos_var())
+        print("Похідна від заданого виразу :", cos_differed_var)
+
+
+choosing_method = int(input("Введіть 1 (якщо sin) чи 2 (якщо cos) для обрання відповідного режиму : "))
+if choosing_method is 1:
+    choosed_algo = DiffSinFromVariable()
+    choosed_algo.sin_diff_var()
+if choosing_method is 2:
+    choosed_algo = DiffCosFromVariable()
+    choosed_algo.cos_diff_var()
