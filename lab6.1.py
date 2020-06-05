a8adb4413611d0261ff2844e5e7768e16a49a3aa
insane_dude <insane.dude.btw@gmail.com>
2020-06-05 20:13:06 +0300

    sin from variable almostly made

---
+++ lab6.1.py
@@ -0,0 +1,64 @@
+import math
+import sympy as sym
+from abc import ABC, abstractmethod
+
+
+class MethodClass(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def used_var(self):
+        pass
+
+    @abstractmethod
+    def sin_var(self):
+        pass
+
+    @abstractmethod
+    def sin_diff_var(self):
+        pass
+
+    @abstractmethod
+    def cos_var(self):
+        pass
+
+    @abstractmethod
+    def cos_diff_var(self):
+        pass
+
+
+class CreatingVariable(MethodClass):
+    def used_var(self):
+        used_variable = sym.Symbol(input("Введіть змінну, яку хочете використовувати : "))
+        input_expr = input("Введіть вираз із вашою змінною : ")
+        return input_expr
+
+    def sin_var(self):
+        pass
+
+    def sin_diff_var(self):
+        pass
+
+    def cos_var(self):
+        pass
+
+    def cos_diff_var(self):
+        pass
+
+
+class SinFromVariable(CreatingVariable):
+    def sin_var(self):
+        sin_from_variable = sym.sin(self.input_expr)
+        print("sin від вашою змінної :", sin_from_variable)
+        return sin_from_variable
+
+
+class DiffSinFromVariable(SinFromVariable):
+    def sin_diff_var(self):
+        sin_differed_var = sym.diff(self.sin_var())
+        print("Похідна від sin :", sin_differed_var)
+
+
+k = DiffSinFromVariable()
+k.sin_diff_var()
---
+++ test.py
@@ -0,0 +1,8 @@
+import sympy as sym
+
+k = sym.Symbol("x")
+print(k)
+d = sym.sin(k)
+print(d)
+l = sym.diff(d)
+print(l)
